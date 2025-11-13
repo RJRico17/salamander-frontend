9148bfbc81146cd3ea8d00222085ac1b1ed43ba1
rjfredr <fredrickalexander1727@gmail.com>
2025-11-13 13:40:30 -0800

    header footer and upload page

---
+++ app/Components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <div className="footer">
+            <p>RJ & Rudolf SDEV355 @ 2025</p>
+        </div>
+    )
+}
---
+++ app/Components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return(
+        <div className="header">
+            <p>Salamnder Video Processor</p>
+            <nav>
+                <button>Upload Video</button>
+                <button>View Videos</button>
+                <button>Process Video</button>
+                <button>Processed</button>
+            </nav>
+        </div>
+    )
+}
